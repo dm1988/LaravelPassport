258d4eb1ddac2b6263e6ae82b8f3f4d5a7b23aef
David Gonzalez <dm1988@msn.com>
2024-04-28 00:10:12 -0500

    test

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,20 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth'])->name('dashboard');
+Route::get('/dashboard/clients', function () {
+    return view('clients', [
+        'clients' => $request->user()
+    ]);
+})->middleware(['auth'])->name('dashboard.clients');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
